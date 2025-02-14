df6def68a1bcfcb2cb250a41576f6e09d9e2fd01
swathireddy369 <swathiamaravadi53@gmail.com>
2025-02-14 16:36:08 -0500

    installation and config for react and tailwind

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="flex items-center justify-center min-h-screen ">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
