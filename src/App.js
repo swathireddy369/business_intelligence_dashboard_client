494b5a8b386fb11e7433850da249ea1607ca8bb1
swathireddy369 <swathiamaravadi53@gmail.com>
2025-02-15 11:10:03 -0500

    test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
       <div className="flex items-center justify-center min-h-screen ">
       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
     </div>
-      </header>
+      {/* </header> */}
     </div>
   );
 }
